c61596073753262ba45e19251eeb344512f78de8
soyikimm <flamingo7763@gmail.com>
2021-09-27 17:41:52 +0900

    JS: navbar menu

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 })
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', () => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) { //링크가 있는경우에만 수행되게함
+      return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+});
