408bc7ba946ecc9c627a6a3fb50e45f2772e89d4
soyikimm <flamingo7763@gmail.com>
2021-10-10 20:05:29 +0900

    comment update

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 'use strict';
 
+
+// 스크롤바를 원하는 포지션으로 내렸을 때 디자인변경 동작
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
+// 넷바의 높이값을 알아내는 함수
 document.addEventListener('scroll', () => {
-    if(window.scrollY > navbarHeight) {
+    if(window.scrollY > navbarHeight) { //scroll이 navhight이상으로 발생
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
@@ -11,17 +14,17 @@ document.addEventListener('scroll', () => {
     navbarMenu.classList.remove('open');
 })
 
-//Navbar toggle
+//넷바 토글
 const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
 navbarToggleBtn.addEventListener('click', ()=> {
   navbarMenu.classList.toggle('open');
 });
 
 
-// Handle scrolling when tapping on the navbar menu
+//넷바메뉴를 클릭했을때 원하는 섹션으로 스크롤링
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', () => {
-    const target = event.target;
+    const target = event.target; //타겟=클릭한요소
     const link = target.dataset.link;
     if (link == null) { //링크가 있는경우에만 수행되게함
       return;
@@ -29,29 +32,33 @@ navbarMenu.addEventListener('click', () => {
     scrollIntoView(link);
 });
 
+
+//Contact me버튼 활성화
 const homeContact = document.querySelector('.home__contact');
 homeContact.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
 
-// Make home slowly fadeout
+// 홈부분 천천히 흐려지게하기
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-// Projects
+// My work Projects
 const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
 workBtnContainer.addEventListener('click', (e) => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  // 숫자를 눌러도 활성화가 되게 or로 해준다.
+  // dataset에 filter가 없으면 parentNode에있는 dataset filter사용
   if (filter == null) {
     return;
   }
 
-// Remove selection from the previous item and select the new one
+// My work에서 클릭된 active로 이동하는 기능
   const active = document.querySelector('.category__btn.selected');
   if (active != null) {
     active.classList.remove('selected');
@@ -63,31 +70,34 @@ workBtnContainer.addEventListener('click', (e) => {
     projects.forEach((project) => {
       console.log(project.dataset.type);
       if (filter === '*' || filter === project.dataset.type) {
-        project.classList.remove('invisible');
+        //전부다 or 필터매칭
+        project.classList.remove('invisible'); //필터가 매칭되면 가리는걸 없앰
       } else {
         project.classList.add('invisible');
       }
     });
     projectContainer.classList.remove('anim-out');
-  }, 300);
+  }, 300); // 0.3초가 지나면 opacity 0 -> 1
 });
 
 
-// show "arrow up" button when scrolling down
+// 스크롤링을 내릴때 위로올라가는 화살버튼
 const arrowUp = document.querySelector('.arrow-up');
 document.addEventListener('scroll', ()=> {
-  if(window.scrollY > homeHeight /2){
+  if(window.scrollY > homeHeight /2){ //홈화면 반이 지낫을때 arrow 보여짐
     arrowUp.classList.add('visible');
   } else{
     arrowUp.classList.remove('visible');
   }
 });
 
-// Handle click on the "arrow up" button
+// arrow up 버튼활성화
 arrowUp.addEventListener('click', ()=> {
   scrollIntoView('#home');
 });
 
+
+//부드럽게 이동하는 스크롤링 함수
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
